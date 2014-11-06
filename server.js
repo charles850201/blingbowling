49ada496eb7fbdc17e9e2b1026f125ab6c647822
charles850201 <charles@bluewavesoft.com>
2014-11-07 03:13:06 +0900

    asdfasdf

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var http = require('http');
 var port = process.env.port || 1337;
 
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
 
-res.write('<p>Hello from ' + process.env.CloudProvider +' </p>');
-res.write('<ul>');
-res.write('<li><label>Service Bus Namespace:</label>'+ process.env.ServiceBusConnectionString +' </li>');
-res.write('<li><label>Storage Connection String:</label>'+ process.env.StorageConnectionString +' </li>');
-res.write('</ul>');
-
-res.write('<p>Happy Clouding!</p>');
+res.write('<p>Happy World!</p>');
 
 res.end('<p>'+ process.env.Developer +'</p>');
 
